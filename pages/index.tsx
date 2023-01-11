e3eb0c9f8f02eb6813db65f086755584ee4ccfff
Eduard Canal <eduard.canal.m@gmail.com>
2023-01-11 20:51:42 +0100

    added some responsiveness to MovieList.module.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Tmdb from '../services/tmdb.services'
 import { useAppDispatch } from '../store/hooks'
 import { addPopularMovies, addUpcomingMovies } from '../redux/movieSlice'
 import { movieListProps, movieReduced } from '../interface/movie'
+import MovieList from '../components/AppMovieList/MovieList'
+import styles from './home.module.css'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,14 @@ export default function Home({ popularMovies, upcomingMovies }: movieListProps) 
 
   return (
     <>
-      <h1></h1>
+    <div className={styles.main}>
+      <h1>Popular</h1>
+      <MovieList moviesToList={popularMovies} />
+      <h1>Upcoming</h1>
+      <MovieList moviesToList={upcomingMovies} />
+      <h1>Favorites</h1>
+      <p>Comming soon</p>
+    </div>
     </>
   )
 }
--- pages/upcoming/index.tsx
+++ pages/upcoming/index.tsx
@@ -1,6 +1,4 @@
 import MovieList from "../../components/AppMovieList/MovieList"
-// import { movieListProps, movieReduced } from "../../interface/movie"
-// import Tmdb from "../../services/tmdb.services"
 import { useAppSelector } from "../../store/hooks"
 
 export default function Upcoming() {
