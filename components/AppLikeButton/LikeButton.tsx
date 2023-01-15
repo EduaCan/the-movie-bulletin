d9222a2d7d24a2acd163f3b6aa41bedf4d87c8dc
yebrai <ivansevv@gmail.com>
2023-01-15 19:33:04 +0100

    added toast

--- components/AppLikeButton/LikeButton.tsx
+++ components/AppLikeButton/LikeButton.tsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
 import { BsSuitHeart, BsSuitHeartFill } from 'react-icons/Bs'
 import { movieDetailsProps } from '../../interface/movie'
 import { addFavouriteMovies } from '../../redux/favouriteSlice'
-import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { useAppDispatch } from '../../store/hooks'
 import styles from './LikeButton.module.css'
 
-export default function LikeButton({details, filteringFavourites }: movieDetailsProps) {
+
+export default function LikeButton({details, filteringFavourites, success }: movieDetailsProps) {
     const dispatch = useAppDispatch()
 
     
     const handleLikeButto = () => {
         dispatch(addFavouriteMovies(details))
+        success(details.id, details.title)
     }
 
 
     
     return (
         <button className={styles.likeButton} onClick={handleLikeButto}>
+
             {filteringFavourites(details.id).length !== 0 ?
                 <BsSuitHeartFill size={30} color={'#2196f3'} /> :
                 <BsSuitHeart size={30} color={'#2196f3'} />}
--- components/AppMovieList/MovieList.tsx
+++ components/AppMovieList/MovieList.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from "../../store/hooks";
 import LikeButton from "../AppLikeButton/LikeButton";
 import Rating from "../AppRating/Rating";
 import styles from './MovieList.module.css'
+import toast, { Toaster } from "react-hot-toast";
 
 export default function MovieList({ moviesToList, title }: movieListReceivedProps) {
     const router = useRouter()
@@ -14,18 +15,22 @@ export default function MovieList({ moviesToList, title }: movieListReceivedProp
     const {favouriteMovies} = useAppSelector((state) => state.favouriteMovies.favouriteMovies)
 
     const filteringFavourites = (id: number) => {
-     return favouriteMovies.filter((movie: movieReduced)  =>{
-         if (movie.id === id) {
-           return true
-         }})
- 
+     return favouriteMovies.filter((movie: movieReduced)  => movie.id === id)}
+
+     const success = (id: number, title: string) => {
+     if (filteringFavourites(id).length === 0) {
+         toast(`${title} 
+         has been added to favourite list`, { icon: "✔️", style: {background: '#2196f3', color: 'white'} } );
+     } else {toast(`${title} 
+     Has been removed from favourite list`, { icon: "❌", style: {background: '#2196f3', color: 'white'} })}
     }
-
-
 
     const imageSize = "w500" //move to better place
     return (
         <div className={styles.wrapper}>
+         <div><Toaster
+  position="top-right"
+/></div>
             <div className={styles.titleContainer}>
                 <div className={styles.mainTitle}>
                     <h1>{title}</h1>
@@ -43,7 +48,7 @@ export default function MovieList({ moviesToList, title }: movieListReceivedProp
                         }
                         return (
                             <div key={movie.id} className={styles.cardContainer}  >
-                                <LikeButton details={movie} filteringFavourites={filteringFavourites}/>
+                                <LikeButton details={movie} filteringFavourites={filteringFavourites} success={success}/>
                                 <Link href={`/movie/${movie.id}`} className={styles.linkImage}>
                                     <Image src={`https://image.tmdb.org/t/p/${imageSize}${movie.poster_path}`} alt={movie.title} fill={true} className={styles.cardImage} />
                                 </Link>
